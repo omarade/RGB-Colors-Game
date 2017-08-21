5724b843dbcca79bb248e7ad9167b8e680e5a922
Omar Abou Dehn <3omar.a.de@gmail.com>
2017-08-21 20:31:11 +0200

    Done with the Hard Mode

--- colorGame.js
+++ colorGame.js
@@ -1,4 +1,9 @@
-var squares = document.querySelectorAll(".square")
+let squares = document.querySelectorAll(".square")
+let CD = document.querySelector("#CD")
+let reBtn = document.querySelector("#newColors")
+let head = document.querySelector("#firstH")
+let result = document.querySelector("#result")
+let rightColor;
 
 function makeRandomColor() {
 	var r = "rgb(" + Math.floor(Math.random() * 256)
@@ -8,6 +13,43 @@ function makeRandomColor() {
 	return color	
 }
 
+function checkPressedColor(){
+	if(this.style.backgroundColor === rightColor){
+		result.textContent = "CORRECT"
+		head.style.backgroundColor = rightColor
+		squares.forEach(function(square){
+			square.classList.remove("fadeOut")
+			square.style.backgroundColor = rightColor
+		})
+	}
+	else{
+		result.textContent = "TRY AGAIN"
+		this.classList.add("fadeOut")
+	}
+}
+
+function giveColors () {
+	squares.forEach(function(square){
+		square.style.backgroundColor = makeRandomColor()
+	})	
+}
+
+function rightColorPicker() {
+	rightColor = squares[Math.floor(Math.random() * 6)].style.backgroundColor
+	CD.textContent = rightColor
+}
+
+giveColors()
+
+rightColorPicker()
+
 squares.forEach(function(square){
-	square.style.backgroundColor = makeRandomColor()
+	square.addEventListener("click", checkPressedColor)
+})
+
+reBtn.addEventListener("click", function(){
+	giveColors()
+	rightColorPicker()
+	result.textContent = ""
+	head.style.backgroundColor = "rgb(16, 140, 117)"
 })
